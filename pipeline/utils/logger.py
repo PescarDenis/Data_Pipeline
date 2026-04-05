3df223f6098a4b061a9a14c425f9aec166e939d5
PescarDenis <pescardeniz@yahoo.com>
2026-04-05 20:51:51 +0300

    feat(util): create a logger to print messages during pipeline stages

--- pipeline/utils/logger.py
+++ pipeline/utils/logger.py
@@ -3,3 +3,6 @@ import datetime
 This will be passed wherever we want to print a message
 during the pipeline stages
 """
+def log(message  : str):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}]:  {message}")
